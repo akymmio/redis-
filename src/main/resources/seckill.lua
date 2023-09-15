d2f9ce5887728d540a4d7fe587f56c4fb40efdc7
mio <2455461086@qq.com>
2023-09-15 20:59:02 +0800

    消息队列优化秒杀

--- src/main/resources/seckill.lua
+++ src/main/resources/seckill.lua
@@ -7,7 +7,8 @@
 local voucherId=ARGV[1]
 --用户id
 local userId=ARGV[2]
-
+--订单id
+local orderId=ARGV[3]
 --库存key
 local stockKey='seckill:stock:' .. voucherId
 --订单key
@@ -23,8 +24,9 @@ if(redis.call('sismember',orderKey,userId)==1) then
     return 2
 end
 
-
 --扣减库存，下单
 redis.call('incrby',stockKey,-1)
 redis.call('sadd',orderKey,userId)
+--发送消息到队列,队列名称stream.order
+redis.call('xadd','stream.orders','*','voucherId',voucherId,'userId',userId,'id',orderId)
 return 0
